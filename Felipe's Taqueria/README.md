9a19ad72482b9db52ff8d1164d1a5dd9e5f8abcd
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2023-12-17 05:01:25 +0100

    order case insensitive

--- Felipe's Taqueria/README.md
+++ Felipe's Taqueria/README.md
@@ -6,7 +6,7 @@ The Felipe's Taqueria is a food store in the United State, It has an online oder
 
 <p> Here in this code, we are handeling customers orders and taking charge of customers needs from our menu list. Customers get to view our menu and make their choice of sellecting from our menu and we calculate the cost of each item sellected </p>
 
-~ If item not currently available at the moment an issue is raised and customers are been asked to coose another item from the menu list.
+~ If item not currently available at the moment an issue is raised and customers are been asked to coose another item from the menu list. </br>
 ~ Customer enters the keyword 'done' after completed sellection
 
 <h3>Issue</h3>
--- Felipe's Taqueria/felipes_taqueria.py
+++ Felipe's Taqueria/felipes_taqueria.py
@@ -7,7 +7,7 @@ menu = {
         "Nachos": 11.00,
         "Quesadilla": 8.50,
         "Super Burrito": 8.50,
-        "super Quesadilla": 9.50,
+        "Super Quesadilla": 9.50,
         "Taco": 3.00,
         "Tortilla Salad": 8.00
     }
@@ -18,9 +18,25 @@ menu = {
 def place_order(menu):
     total_price = 0
     while True:
-        order = input('What is your order: ')
+        order = input('What is your order: ').strip()
         if order.lower() == 'done':
-            break
+            break 
+        for ch in order:
+            if ch.lower() or ch.isupper() in order:
+                # for ch in order:
+                #     if ch.replace(" ", "") == ch.replace(" ", ""):
+                #         order.append(order.capitalize())
+                order = order.capitalize()
+        # for order in menu:
+        #     if order.replace(" ", "") == order.replace(" ", ""):
+        #         order.append(order.capitalize())
+        #         found = True
+        #         break
+        #     if not found:
+        #         order = input("Sorry that item is not on the menu.")
+
+                
+            # Alright I already handled the lowercase characters, but not handeling splitted characters
         if order in menu:
             total_price += menu[order]
             print(f"{order.capitalize()} added to the order. Total price: ${total_price:.2f}")
